864a954c3842dda7d2c8820a07879728a7824c2e
root <dnbennett@hotmail.co.uk>
2016-12-14 23:23:31 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: "app/scss/*.scss",
+        tasks: ['sass']
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          // destination          // source file
+          "app/css/styles.css" : "app/scss/styles.scss"
+        }
+      }
+    },
+
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "app/css/*.css",
+            "*.html"
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
+
+// broswerSync localhost:3000
